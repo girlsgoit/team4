eef1a13f3c7848af057bf0a0e894c376b0573ff0
Nastea Popova <nastea.popova1907@gmail.com>
2023-07-28 18:12:12 +0300

    register.vue

--- dinamic/MEvent/src/components/Login.vue
+++ dinamic/MEvent/src/components/Login.vue
@@ -29,6 +29,7 @@
                 </div>
             </div>
         </div>
+        
 
     </header>
     </div>
--- dinamic/MEvent/src/views/RegisterView.vue
+++ dinamic/MEvent/src/views/RegisterView.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
-        <h2>RegisterView</h2>
+        <Register/>
     </div>
 </template>
 
 
 <script>
+    import Register from "../components/Register.vue"
+    export default {
+        name: "RegisterView",
+        components: {
+            Register
+        }}
 
 </script>
 
